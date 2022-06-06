85c8aafc1a44adc919b0985c0188e900f4dea437
skwuncut <71997323+skwuncut@users.noreply.github.com>
2022-06-06 15:38:29 +0800

    Add files via upload

---
+++ WindowsFormsApp1(final McDonload))/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.品名 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.數量 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.單價 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.品名,
+            this.數量,
+            this.單價});
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.Location = new System.Drawing.Point(287, 91);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 53;
+            this.dataGridView1.Size = new System.Drawing.Size(573, 245);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // ID
+            // 
+            this.ID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.Width = 50;
+            // 
+            // 品名
+            // 
+            this.品名.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.品名.HeaderText = "品名";
+            this.品名.Name = "品名";
+            this.品名.Width = 250;
+            // 
+            // 數量
+            // 
+            this.數量.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.數量.HeaderText = "數量";
+            this.數量.Name = "數量";
+            // 
+            // 單價
+            // 
+            this.單價.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.單價.HeaderText = "單價";
+            this.單價.Name = "單價";
+            // 
+            // button3
+            // 
+            this.button3.Image = global::WindowsFormsApp1.Properties.Resources.p4;
+            this.button3.Location = new System.Drawing.Point(28, 469);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 124);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = global::WindowsFormsApp1.Properties.Resources.p2;
+            this.button2.Location = new System.Drawing.Point(24, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 137);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Image = global::WindowsFormsApp1.Properties.Resources.p1;
+            this.button1.Location = new System.Drawing.Point(31, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 138);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(38, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "BLT 安格斯黑牛堡";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(41, 392);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "BLT 辣脆鷄腿堡";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(45, 595);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "鷄腿中薯豪飽餐";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(65, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 22);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "NT$119";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(70, 617);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "NT$204";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(70, 417);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 22);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "NT$119";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(322, 356);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 36);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(654, 356);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(89, 36);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "總計";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(724, 408);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 25);
+            this.label6.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label8.Location = new System.Drawing.Point(651, 408);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 25);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Total:";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(463, 358);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(89, 34);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Delete";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label9.Location = new System.Drawing.Point(774, 408);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 25);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "元";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(287, 452);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(300, 153);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "請選擇時段:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.radioButton1.Location = new System.Drawing.Point(22, 41);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(62, 35);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "上午";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.radioButton2.Location = new System.Drawing.Point(149, 41);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(62, 35);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "下午";
+            this.radioButton2.UseVisualStyleBackColor = true;
+           // this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(958, 723);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(1);
+            this.Name = "Form1";
+            this.Text = "國立勤益科技大學";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 品名;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 數量;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 單價;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ WindowsFormsApp1(final McDonload))/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            radioButton1.Checked=true;
+            radioButton2.Checked = false;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[,] rows = new string[,]{ // 1   2     3     4 
+                                             
+                                              {"1","BLT 安格斯黑牛堡","1","119"}
+                                          };
+
+
+            for (int i = 0; i < rows.GetLength(0); i++)// array rows
+            {
+                string[] row = new string[rows.GetLength(1)];
+
+                for (int j = 0; j < rows.GetLength(1); j++)
+                {
+                    row[j] = rows[i, j];
+                }
+                dataGridView1.Rows.Add(row);
+
+
+
+
+                //string[] c = { "benz", "bmw" };
+                //string[] m = { "samsung", "iphone" };
+
+                // if (comboBox1.Text == "car")
+                //{
+                // listBox1.Items.Clear();
+                // listBox1.Items.AddRange(c);
+
+                // };
+                // if (comboBox1.Text == "mob")
+                // {
+                //  listBox1.Items.Clear();
+                //  listBox1.Items.AddRange(m);
+
+
+            };
+
+            bool Found = false;
+            
+            if (dataGridView1.Rows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    // if (Convert.ToString(row.Cells[0].Value) == CmbProductName.Text)
+                    if (Convert.ToString(row.Cells[1].Value) == dataGridView1.Name)
+
+                        {
+                        row.Cells[2].Value = Convert.ToString(1 + Convert.ToInt16(row.Cells[2].Value));
+                        Found = true;
+
+
+                    }
+                }
+
+               
+            }
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[,] rows = new string[,]{ // 1   2     3     4 
+                                             {"2","BLT 辣脆鷄腿堡","1","119"}
+
+                                          };
+
+
+            for (int i = 0; i < rows.GetLength(0); i++)// array rows
+            {
+                string[] row = new string[rows.GetLength(1)];
+
+                for (int j = 0; j < rows.GetLength(1); j++)
+                {
+                    row[j] = rows[i, j];
+                }
+                dataGridView1.Rows.Add(row);
+
+
+
+                //string[] c = { "benz", "bmw" };
+                //string[] m = { "samsung", "iphone" };
+
+                // if (comboBox1.Text == "car")
+                //{
+                // listBox1.Items.Clear();
+                // listBox1.Items.AddRange(c);
+
+                // };
+                // if (comboBox1.Text == "mob")
+                // {
+                //  listBox1.Items.Clear();
+                //  listBox1.Items.AddRange(m);
+
+
+            };
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[,] rows = new string[,]{ // 1   2     3     4 
+                                             {"3","鷄腿中薯豪飽餐","1","204"}
+
+                                          };
+
+
+            for (int i = 0; i < rows.GetLength(0); i++)// array rows
+            {
+                string[] row = new string[rows.GetLength(1)];
+
+                for (int j = 0; j < rows.GetLength(1); j++)
+                {
+                    row[j] = rows[i, j];
+                }
+                dataGridView1.Rows.Add(row);
+
+
+
+                //string[] c = { "benz", "bmw" };
+                //string[] m = { "samsung", "iphone" };
+
+                // if (comboBox1.Text == "car")
+                //{
+                // listBox1.Items.Clear();
+                // listBox1.Items.AddRange(c);
+
+                // };
+                // if (comboBox1.Text == "mob")
+                // {
+                //  listBox1.Items.Clear();
+                //  listBox1.Items.AddRange(m);
+
+
+            };
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            for (int i = 0; i < dataGridView1.Rows.Count; ++i)
+            {
+                sum += Convert.ToInt32(dataGridView1.Rows[i].Cells[3].Value);
+            }
+            label6.Text = sum.ToString();
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow item in this.dataGridView1.SelectedRows)
+            {
+                dataGridView1.Rows.RemoveAt(item.Index);
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked==true)
+            {
+
+                button1.Enabled = true;
+                button2.Enabled = false;
+                button3.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = false;
+                button2.Enabled = true;
+                button3.Enabled = true;
+
+            }
+
+            
+        }
+
+       
+    }
+
+    }
+
